5520f4ac1bf3547e7747f05e8d001eda06cf5ab2
Charu Sneha <charusneha266@gmail.com>
2026-03-14 17:18:53 -0700

    Hybrid search code with rrf ranking (Reciprocal rank fusion)

--- cli/hybrid_search_cli.py
+++ cli/hybrid_search_cli.py
@@ -1,5 +1,5 @@
 import argparse
-from lib.hybrid_search import normalize_scores, weighted_search
+from lib.hybrid_search import normalize_scores, weighted_search, rrf_search
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Hybrid Search CLI")
@@ -14,9 +14,16 @@ def main() -> None:
     weighted_parser.add_argument("--alpha",type=float , default = 0.5, help="Constant to dynamically control weighting between two scores")
     weighted_parser.add_argument("--limit",type=int, default = 5, help="Limit of the answer")
 
+    rrf_parser = subparsers.add_parser(name="rrf_search", help="Command to do rrf search")
+    rrf_parser.add_argument("query",type=str, help="User Query for the search")
+    rrf_parser.add_argument("--k",type=int , default = 60, help="Constant to dynamically control weighting between two scores")
+    rrf_parser.add_argument("--limit",type=int, default = 5, help="Limit of the answer")
+
     args = parser.parse_args()
 
     match args.command:
+        case "rrf_search":
+          rrf_search(args.query, args.k, args.limit)
         case "weighted_search":
           weighted_search(args.query, args.alpha, args.limit)
         case "normalize":
--- cli/lib/hybrid_search.py
+++ cli/lib/hybrid_search.py
@@ -14,6 +14,16 @@ def weighted_search(query, alpha=0.5, limit=5):
     print(f"BM25: {r['bm25_score']}, Semantic: {r['sem_score']}")
     print(r['description'][:100])
 
+def rrf_search(query, k=60, limit=5):
+  movies = load_movies()
+  hs = HybridSearch(movies)
+  results = hs.rrf_search(query, k, limit)
+  for idx, r in enumerate(results[:limit], start=1):
+    print(f"{idx+1} {r['title']}")
+    print(f"RRF Score: {r['rrf_score']}")
+    print(f"BM25 Rank: {r['bm25_score']}, Semantic Rank: {r['sem_score']}")
+    print(r['description'][:100])
+
 class HybridSearch:
     def __init__(self, documents):
         self.documents = documents
@@ -32,11 +42,14 @@ class HybridSearch:
     def weighted_search(self, query, alpha=0.5, limit=5):
           bm25_results = self._bm25_search(query, limit*500)
           sem_results = self.semantic_search.search_chunks(query, limit*500)
-          combined_results = combine_search_results(bm25_results, sem_results)
+          combined_results = combine_search_results(bm25_results, sem_results, alpha)
           return combined_results
 
     def rrf_search(self, query, k, limit=10):
-        raise NotImplementedError("RRF hybrid search is not implemented yet.")
+        bm25_results = self._bm25_search(query, limit*500)
+        sem_results = self.semantic_search.search_chunks(query, limit*500)
+        combined_results = rrf_combine_search_results(bm25_results, sem_results, k)
+        return combined_results
 
 def hybrid_score(bm25_score, sem_score, alpha=0.5):
   return (alpha * bm25_score + (1 - alpha) * sem_score)
@@ -48,7 +61,54 @@ def normalize_search_results(results):
     result['normalized_score'] = norm_scores[idx]
   return results
 
-def combine_search_results(bm25_results, sem_results):
+def rrf_score(rank, k):
+  return 1/ (rank + k)
+
+def rrf_final_score(r1, r2,k):
+  if r1 and r2:
+    return rrf_score(r1, k) + rrf_score(r2, k)
+  return 0.
+
+def rrf_combine_search_results(bm25_results, sem_results, k):
+  scores = {}
+
+  for rank, result in enumerate(bm25_results, start=1):
+    doc_id = result['id']
+    scores[doc_id] = {
+      'doc_id': doc_id,
+      'bm25_rank': rank,
+      'bm25_score':rrf_score(rank, k),
+      'sem_rank': None,
+      'sem_score': None,
+      'title': result['title'],
+      'description': result['document']
+    }
+
+  for rank, result in enumerate(sem_results, start=1):
+    doc_id = result['id']
+    if doc_id not in scores:
+      scores[doc_id] = {
+        'doc_id': doc_id,
+        'bm25_rank': None,
+        'bm25_score':None,
+        'sem_rank': None,
+        'sem_score': None,
+        'title': result['title'],
+        'description': result['document']
+      }
+    scores[doc_id]['sem_rank'] = rank
+    scores[doc_id]['sem_score'] = rrf_score(rank,k)
+  
+  for doc_id in scores.keys():
+    scores[doc_id]['rrf_score'] = rrf_final_score(
+      scores[doc_id]['bm25_rank'],
+      scores[doc_id]['sem_rank'], k
+    )
+  results = sorted(list(scores.values()), key=lambda x: x['rrf_score'], reverse=True )
+  return results
+
+      
+def combine_search_results(bm25_results, sem_result, alpha):
   bm25_norm = normalize_search_results(bm25_results)
   sem_norm = normalize_search_results(sem_results)
 
@@ -75,9 +135,9 @@ def combine_search_results(bm25_results, sem_results):
     combined_norm[doc_id]['sem_score'] = norm['normalized_score']
 
   for k,v in combined_norm.items():
-    combined_norm[k]['hybrid_score'] = hybrid_score(v['bm25_score'], v['sem_score'])
+    combined_norm[k]['hybrid_score'] = hybrid_score(v['bm25_score'], v['sem_score'], alpha)
 
-  results = sorted(combined_norm.values(), key=lambda x: x['hybrid_score'], reverse=True )
+  results = sorted(list(combined_norm.values()), key=lambda x: x['hybrid_score'], reverse=True )
   return results
 
 
